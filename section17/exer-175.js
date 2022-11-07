4b88356bfb9bada05c1cb6ce7de027a4e8f7b2a9
Marco Antonio Torquato <marcotorquato97@gmail.com>
2022-11-07 20:58:58 +0100

    create 175

---
+++ section17/exer-175.js
@@ -0,0 +1,5 @@
+// Let's get some practice creating arrays. Please define a variable called lottoNumbers and set it equal to an array containing any 6 numbers.  It's simple, but we've got to start somewhere!
+
+// Define lottoNumbers below:
+
+let lottoNumbers = [3,5,6,9,12,15];
