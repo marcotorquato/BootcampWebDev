584a8af3a4f8c460cb6280db00e4bc14cea8e3f3
Marco Antonio Torquato <marcotorquato97@gmail.com>
2022-11-15 19:06:06 +0100

    Update exer192.js

---
+++ section19/exer192.js
@@ -0,0 +1,12 @@
+// Let's try writing a for loop that counts down, rather than up. Please write a for loop that prints the following numbers (in this order):
+
+// 25
+// 20
+// 15
+// 10
+// 5
+// 0
+
+for (let num = 25; num >=0; num -=5) {
+    console.log(num)
+}
